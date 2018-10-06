dabf818953ea8eb253a6f91a043fb9f7fc1c46e7
Ahmed <ahmed91abbas@hotmail.com>
2018-10-06 18:31:39 +0200

    better url fetching and removed duplicates

--- WeDi.py
+++ WeDi.py
@@ -8,6 +8,7 @@ class services:
         self.site = site
         self.create_dest_folders(path)
         self.img_urls = []
+        self.img_types = ['jpg', 'jpeg', 'png', 'gif']
         self.connect()
         self.extract_urls()
 
@@ -22,30 +23,33 @@ class services:
 
     def extract_urls(self):
         # self.urls = self.soup.find_all('a')
-        self.urls = re.findall('"((http|ftp)s?://.*?)"', self.response.text)
+        self.urls = re.findall('["\']((http|ftp)s?://.*?)["\']', self.response.text)
         #add image urls
-        img_types = ['jpg', 'jpeg', 'png', 'gif']
         for url in self.urls:
             url = url[0]
-            if (len(url) > 4 and (url[-4:] in img_types or url[-3:] in img_types)):
+            if (len(url) > 4 and (url[-4:] in self.img_types or url[-3:] in self.img_types)):
                 self.img_urls.append(url)
 
     def extract_images(self):
         img_tags = self.soup.find_all('img')
 
         for img in img_tags:
-            print(img)
+            url = ''
             if ' src=' in str(img):
-                self.img_urls.append(img['src'])
+                url = img['src']
             elif ' data-src=' in str(img):
-                self.img_urls.append(img['data-src'])
+                url = img['data-src']
+            if (len(url) > 4 and (url[-4:] in self.img_types or url[-3:] in self.img_types)):
+                self.img_urls.append(url)
 
     def output_results(self):
         #output for images
+        self.img_urls = set(self.img_urls)
         for url in self.img_urls:
             print(url)
             filename = re.search(r'/([\w_-]+[.](jpg|gif|png|jpeg))', url)
             if filename == None:
+                print(url)
                 filename = re.sub('[^0-9a-zA-Z]+', '', url) + ".jpg"
             else:
                 filename = filename.group(1)
@@ -57,7 +61,7 @@ class services:
                 f.write(response.content)
 
 if __name__ == "__main__":
-    site = 'https://www.youtube.com/watch?v=lBdnti4_UUg'
+    site = 'https://www.youtube.com/watch?v=GhlLy2elSlI'
     services = services(site, "")
     services.extract_images()
     services.output_results()

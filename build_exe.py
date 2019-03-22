e78db36a9fc31efa138f540098734a9746ef1214
Ahmed <ahmed91abbas@hotmail.com>
2019-03-22 11:15:39 +0100

    build exe now creates a shortcut for the main.exe

--- build_exe.py
+++ build_exe.py
@@ -1,5 +1,6 @@
 import subprocess
 import os, sys, shutil
+from win32com.client import Dispatch
 
 def remove_file(file):
     try:
@@ -12,6 +13,16 @@ def remove_dir(dir):
         shutil.rmtree(dir)
     except:
         pass
+
+def create_shourtcut(save_to, wDir, exe_name):
+    target_file = os.path.join(wDir, exe_name)
+    save_to = os.path.join(save_to, "WeDi.lnk")
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(save_to)
+    shortcut.Targetpath = target_file
+    shortcut.WorkingDirectory = wDir
+    shortcut.IconLocation = target_file
+    shortcut.save()
 
 def clean_up():
     file = "main.spec"
@@ -44,6 +55,7 @@ def replace_local_copy():
     shutil.copytree("dist\\main", exe_path)
     download_path = os.path.join(dist, "wedi_downloads")
     os.mkdir(download_path)
+    create_shourtcut(dist, exe_path, "main.exe")
 
 print("Cleaning up old files...\n")
 clean_up()

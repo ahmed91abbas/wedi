01eb79f4e3b4922667751fc911fc48d6f92d5ddf
Ahmed <ahmed91abbas@hotmail.com>
2020-01-01 21:18:21 +0100

    Change from relative paths to absolute ones

--- build_exe.py
+++ build_exe.py
@@ -1,6 +1,14 @@
 import subprocess
 import os, sys, shutil
+from os.path import join
 from win32com.client import Dispatch
+from environs import Env
+
+def path_join(list_of_names):
+    path = ""
+    for name in list_of_names:
+        path = join(path, name)
+    return path
 
 def remove_file(file):
     try:
@@ -33,34 +41,44 @@ def clean_up():
         remove_dir(dir_)
 
 def call_process():
-    icon = os.path.join("textures", "icon.ico")
+    icon = join("textures", "icon.ico")
     params = ['pyinstaller', '--icon=' + icon, '--noconsole', 'main.py']
     subprocess.call(params)
 
-def add_files():
-    shutil.copyfile("settings.sav", "dist\\main\\settings.sav")
-    shutil.copyfile("certifi\\cacert.pem", "dist\\main\\certifi\\cacert.pem")
-    folders = ["textures", "drivers", "ffmpeg_win"]
+def add_files(root_path):
+    shutil.copyfile("settings.sav", path_join([root_path, "dist", "main", "settings.sav"]))
+    shutil.copyfile(join("certifi", "cacert.pem"), path_join([root_path, "dist", "main", "certifi", "cacert.pem"]))
+    folders = ["textures", "drivers", "ffmpeg_win", "domains"]
     for folder in folders:
-        dist = "dist\\main\\" + folder
+        dist = path_join([root_path, "dist", "main", folder])
         remove_dir(dist)
         shutil.copytree(folder, dist)
 
-def replace_local_copy():
-    dist = "C:\\Users\\Ahmed\\Desktop\\Others\\wedi"
+def replace_local_copy(parent_path, root_path):
+    dist = join(parent_path, "wedi")
     remove_dir(dist)
-    exe_path = os.path.join(dist, "exe_files")
-    shutil.copytree("dist\\main", exe_path)
-    download_path = os.path.join(dist, "wedi_downloads")
+    exe_path = join(dist, "exe_files")
+    shutil.copytree(path_join([root_path, "dist", "main"]), exe_path)
+    download_path = join(dist, "wedi_downloads")
     os.mkdir(download_path)
     create_shourtcut(dist, exe_path, "main.exe")
 
-print("Cleaning up old files...\n")
-clean_up()
-print("\nCreating the exe...\n")
-call_process()
-print("\nAdding missing files...\n")
-add_files()
-print("\nReplacing local copy of wedi...\n")
-replace_local_copy()
-print("\nDone.")
+def remove_leading_slashes(path_str):
+    while path_str[0] in ["/", "\\"]:
+        path_str = path_str[1:]
+    return path_str
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    save_to_parent_path = env("SAVE_TO_PARENT_PATH")
+    root_path = env("ROOT_PATH")
+
+    print("Cleaning up old files...\n")
+    clean_up()
+    print("\nCreating the exe...\n")
+    call_process()
+    print("\nAdding missing files...\n")
+    add_files(root_path)
+    print("\nReplacing local copy of wedi...\n")
+    replace_local_copy(save_to_parent_path, root_path)
+    print("\nDone.")
